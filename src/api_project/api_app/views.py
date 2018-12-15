60e531b74d29faca5929e51c226db370be01bece
aboamer <aboamer.nrc@gmail.com>
2018-12-15 14:06:58 +0200

    v4 Added HellowViewSet with complete fuctionality

--- src/api_project/api_app/views.py
+++ src/api_project/api_app/views.py
@@ -38,6 +38,7 @@ class HelloAPIView(APIView):
 
 class HelloViewSet(viewsets.ViewSet):
     """docstring for HelloView."""
+    serilaizer_class=serializers.HelloSerializer
     def list(self,request):
         a_view = [
         '1- the first in the list',
@@ -46,3 +47,24 @@ class HelloViewSet(viewsets.ViewSet):
         ]
 
         return Response({'message':'Hello form viewsets','a_view':a_view})
+
+    def create(self,request):
+        serializer = serializers.HelloSerializer(data=request.data)
+        if serializer.is_valid():
+            name = serializer.data.get('name')
+            message = 'A new create message {0}'.format(name)
+            return Response({'message':message})
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def retrive(self, request, pk=None):
+        return Response({'message':'GET method'})
+
+    def update(self,request,pk=None):
+        return Response({'message':'POST method'})
+
+    def partial_update(self,request,pk=None):
+        return Response({'message':'PATCH method'})
+
+    def destroy(self,request,pk=None):
+        return Respone({'message':'DELETE method'})
