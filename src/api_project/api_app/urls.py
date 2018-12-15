1862bd72675ba3af68d1b9566b0d34d0eea28720
aboamer <aboamer.nrc@gmail.com>
2018-12-15 17:30:09 +0200

    v5 added login API

--- src/api_project/api_app/urls.py
+++ src/api_project/api_app/urls.py
@@ -8,6 +8,7 @@ from rest_framework.routers import DefaultRouter
 router = DefaultRouter()
 router.register('Hello-ViewSet', views.HelloViewSet,base_name='Hello-ViewSet')
 router.register('profile', views.UserProfileViewSet)
+router.register('login',views.LoginViewSet,base_name='login')
 
 urlpatterns =[
 url(r'^hello-api/',views.HelloAPIView.as_view()),
