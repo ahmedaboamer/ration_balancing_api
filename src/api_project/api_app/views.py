202d59cabfcef16e6cc0009e37bc06b389856499
aboamer <aboamer.nrc@gmail.com>
2018-12-15 02:39:47 +0200

    created Hello-api view

--- src/api_project/api_app/views.py
+++ src/api_project/api_app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from rest_framework.views import APIView
+from rest_framework.response import Response
 # Create your views here.
+
+class HelloAPIView(APIView):
+    """docstring for HelloAPI."""
+    def get(self,request,format=None):
+        an_apiview = ['1- the first api',
+        '2- the second api',
+        '3- the third api']
+
+        return Response({'message':'hello','an_apiview':an_apiview})
