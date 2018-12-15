88fd4a1e02899d5d21e7f36d0e71944d0d0aab6c
aboamer <aboamer.nrc@gmail.com>
2018-12-15 03:26:13 +0200

    v4 create get post put patch delete API

--- src/api_project/api_app/views.py
+++ src/api_project/api_app/views.py
@@ -1,13 +1,36 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+
+from . import serializers
 # Create your views here.
 
 class HelloAPIView(APIView):
     """docstring for HelloAPI."""
+    serializer_class = serializers.HelloSerializer
+
     def get(self,request,format=None):
         an_apiview = ['1- the first api',
         '2- the second api',
         '3- the third api']
 
         return Response({'message':'hello','an_apiview':an_apiview})
+
+    def post(self, request):
+        serializer = serializers.HelloSerializer(data=request.data)
+        if serializer.is_valid():
+            name = serializer.data.get('name')
+            message = 'Hello {0}'.format(name)
+            return Response({'message':message})
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self,request,pk=None):
+        return Response({'message':'put method'})
+
+    def patch(self,request,pk=None):
+        return Response({'message':'patch method'})
+
+    def delete(self,request,pk=None):
+        return Response({'message':'delete method'})
